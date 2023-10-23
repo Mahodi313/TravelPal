7914c074b2c0c8cf7f60891639b4ddb58e6bb98f
Mahodi313 <Mehdiali15@outlook.com>
2023-10-23 17:39:29 +0200

    IUser/Admin/User - Created - Enums added

---
+++ TravelPal/User Models/Admin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Enums;
+
+namespace TravelPal.User_Models;
+
+public class Admin : IUser
+{
+    public string Firstname { get; set;}
+    public string Lastname { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public Country Location { get; set; }
+
+    public Admin(string firstname, string lastname, string username, string email, string password, Country location)
+    {
+        Firstname = firstname;
+        Lastname = lastname;
+        Username = username;
+        Email = email;
+        Password = password;
+        Location = location;
+    }
+}
