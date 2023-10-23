e2e17ce518662f3b84bc02e3d459aad337eb9fea
Mahodi313 <Mehdiali15@outlook.com>
2023-10-23 18:07:58 +0200

    Update Country.cs

--- TravelPal/Enums/Country.cs
+++ TravelPal/Enums/Country.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TravelPal.Enums
+namespace TravelPal.Enums;
+
+public enum Country
 {
-    internal class Country
-    {
-    }
+
 }
