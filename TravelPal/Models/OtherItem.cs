8a6483e44c48d268a5651a80e2acbd084d2c7e94
Mahodi313 <Mehdiali15@outlook.com>
2023-10-24 16:39:35 +0200

    Packinglist-subclasses added

---
+++ TravelPal/Models/OtherItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPal.Models;
+
+public class OtherItem : PackingListItem
+{
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+
+    public OtherItem(string name, int quantity)
+    {
+        Name = name;
+        Quantity = quantity;
+    }
+
+    public string GetInfo()
+    {
+        return $"{Name} - Quantity: {Quantity}";
+    }
+}
---
+++ TravelPal/Models/TravelDocument.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPal.Models
+{
+    public class TravelDocument : PackingListItem
+    {
+        public string Name { get; set; }
+        public bool Required { get; set; }
+
+        public TravelDocument(string name, bool required)
+        {
+            Name = name;
+            Required = required;
+        }
+
+        public string GetInfo()
+        {
+            if (Required) 
+            {
+                return $"{Name} - Required";
+            }
+            else 
+            {
+                return $"{Name} - Is not required";
+            }
+        }
+    }
+}
