1e51461d41cb4cb630397b169f2f98bf016df48d
Mahodi313 <Mehdiali15@outlook.com>
2023-10-24 18:37:33 +0200

    AddedManagersUserAndTravel

---
+++ TravelPal/Managers/TravelManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Models;
+
+namespace TravelPal.Managers
+{
+    public static class TravelManager
+    {
+        public static List<Travel> travels = new();
+
+        public static void AddTravel(Travel travelToAdd) 
+        {
+            //TODO: IMPLEMENT LOGIC FOR ADDING TRAVEL
+        }
+
+        public static void RemoveTravel(Travel travelToRemove) 
+        {
+            //TODO: IMPLEMENT LOGIC FOR REMOVING TRAVEL
+        }
+    }
+}
---
+++ TravelPal/Managers/UserManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Enums;
+using TravelPal.User_Models;
+
+namespace TravelPal.Managers;
+
+public static class UserManager
+{
+    private static int _idCounter = 2;
+
+    public static List<IUser> users = new()
+    {
+        new Admin("Mahdi", "Ali", "admin", "Mahdi.Ali@edu.newton.se", "password", Enums.Country.Sweden),
+        new User(1, "Dr", "Horse", "user", "Dr.Horse@edu.newton.se", DateTime.Parse("2023-10-24"), "password",Country.Sweden)
+    };
+
+    public static IUser? SignedInUser { get; set; }
+
+    public static bool AddUser(IUser user) 
+    {   
+        //TODO: IMPLEMENT LOGIC FOR ADDING NEW USER
+        return false;
+    }
+
+    public static void RemoveUser(IUser user) 
+    {
+        //TODO: IMPLEMENT LOGIC FOR REMOVING USER
+    }
+    
+    public static bool UpdateUsername(IUser user, string choosenName) 
+    {   
+        //TODO: IMPLEMENT LOGIC FOR UPDATING AND CHECKING USERNAME
+        return false;
+    }
+
+    private static bool ValidateUserName(string userName) 
+    {   
+        //TODO: IMPLEMENT LOGIC FOR CHECKING IF USERNAME IS AVAILABLE
+        return false;
+        
+    }
+    public static bool UpdateEmail(IUser user, string choosenEmail)
+    {
+        //TODO: IMPLEMENT LOGIC FOR UPDATING AND CHECKING EMAIL
+        return false;
+    }
+
+    private static bool ValidateEmail(string email)
+    {
+        //TODO: IMPLEMENT LOGIC FOR CHECKING IF EMAIL IS AVAILABLE
+        return false;
+
+    }
+
+    public static bool SignInUser(string username, string password) 
+    {   
+        //TODO: IMPLEMENT LOGIC FOR CHECKING IF USER CAN SIGN IN
+        return false;
+    }
+
+}
