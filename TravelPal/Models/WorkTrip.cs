cfb674701756fd5d48b3cdc323ad5567e120d74a
Mahodi313 <Mehdiali15@outlook.com>
2023-10-24 17:44:05 +0200

    Added worktripClass

---
+++ TravelPal/Models/WorkTrip.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Enums;
+
+namespace TravelPal.Models
+{
+    public class WorkTrip: Travel
+    {
+
+        public string MeetingDetails { get; set; }
+
+        public WorkTrip(string destination, Country country, int travellers, List<PackingListItem> packinglist, DateTime startDate, DateTime endDate, string meetingDetails) : base(destination, country, travellers, packinglist, startDate, endDate)
+        {
+            MeetingDetails = meetingDetails;
+        }
+
+        public override string GetInfo()
+        {
+            return $"WorkTrip - {Country} - Date: {StartDate.Day}/{StartDate.Month}/{StartDate.Year}";
+        }
+    }
+}
