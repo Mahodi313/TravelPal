3027d9a92b05b1fd0149f06503eeef9caca4df41
Mahodi313 <Mehdiali15@outlook.com>
2023-10-28 19:02:28 +0200

    UpdatedAdminWindow-code-behind

--- TravelPal/AdminWindow.xaml.cs
+++ TravelPal/AdminWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TravelPal.Managers;
+using TravelPal.Models;
+using TravelPal.User_Models;
 
 namespace TravelPal
 {
@@ -22,6 +25,68 @@ namespace TravelPal
         public AdminWindow()
         {
             InitializeComponent();
+
+            foreach (var user in UserManager.users) 
+            {
+                if (user.GetType() == typeof(Admin)) 
+                {
+                    cbUsers.Items.Remove(user);
+                }
+
+                ComboBoxItem item = new ComboBoxItem();
+                item.Content = user.Username;
+                item.Tag = user;
+                cbUsers.Items.Add(item);
+            }
+            
+
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnManageUsers_Click(object sender, RoutedEventArgs e)
+        {
+            // ADD LATER
+        }
+
+        private void btnRemoveTravel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cbUsers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try 
+            {
+                ComboBoxItem? selectedUser = cbUsers.SelectedItem as ComboBoxItem;
+
+                if (selectedUser == null) 
+                {
+                    throw new NullReferenceException("You need to select user before proceeding!");
+                }
+                else 
+                {
+                    User user = (User)selectedUser.Tag;
+
+                    foreach(Travel travel in user.Travels) 
+                    {
+
+                        ListViewItem item = new ListViewItem();
+                        item.Content = travel.GetInfo();
+                        item.Tag = travel;
+
+                        lstUserTravels.Items.Add(item);
+
+                    }
+                }
+            }
+            catch (NullReferenceException ex) 
+            {
+                MessageBox.Show(ex.Message, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
     }
 }
